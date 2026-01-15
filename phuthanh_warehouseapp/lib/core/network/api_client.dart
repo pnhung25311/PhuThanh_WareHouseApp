46e1442a99ccba606ccb2a2525c2699979bbc1d5
pnhung25311 <pnhung25311@gmail.com>
2026-01-15 16:29:19 +0700

    cmmit logo

--- phuthanh_warehouseapp/lib/core/network/api_client.dart
+++ phuthanh_warehouseapp/lib/core/network/api_client.dart
@@ -10,10 +10,10 @@ class ApiClient {
   const ApiClient();
 
   Future<String> getBaseUrl() async {
-    // String localIP = 'http://192.168.1.54:2010/api/';
-    // String puclicIP = 'http://14.224.207.115:2010/api/';
-    String localIP = 'http://192.168.1.11:8080/api/';
-    String puclicIP = 'http://14.224.207.115:8080/api/';
+    String localIP = 'http://192.168.1.54:2010/api/';
+    String puclicIP = 'http://14.224.207.115:2010/api/';
+    // String localIP = 'http://192.168.1.11:8080/api/';
+    // String puclicIP = 'http://14.224.207.115:8080/api/';
     try {
       final url = Uri.parse('http://checkip.amazonaws.com/');
       final result = await http.get(url);
