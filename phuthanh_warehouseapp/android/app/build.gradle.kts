646981e47ec844bc1c8386b961645fe1ba4131dc
pnhung25311 <pnhung25311@gmail.com>
2026-03-24 16:42:32 +0700

    commit test notification

--- phuthanh_warehouseapp/android/app/build.gradle.kts
+++ phuthanh_warehouseapp/android/app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -49,6 +50,10 @@ android {
         }
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
 
 flutter {
