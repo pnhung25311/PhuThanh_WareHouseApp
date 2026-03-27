c893d4b68f80f41c5236d1f24def28a65df0535a
pnhung25311 <pnhung25311@gmail.com>
2026-03-27 09:59:15 +0700

    commit

--- phuthanh_warehouseapp/android/app/build.gradle.kts
+++ phuthanh_warehouseapp/android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 
@@ -54,6 +55,8 @@ android {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+    implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 flutter {
--- phuthanh_warehouseapp/lib/main.dart
+++ phuthanh_warehouseapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:phuthanh_warehouseapp/Screen/auth/LoginScreen.screen.dart';
 import 'package:phuthanh_warehouseapp/service/notification_service.service.dart';
@@ -5,6 +6,7 @@ import 'package:phuthanh_warehouseapp/warehouse/store/AppState.store.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
 
   await NotificationService().init(); // 🔥 thêm dòng này
 
